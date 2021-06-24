1ab74fb3ce189d42c16d0e5b94d4c58e20899d60
Roshan Bisht <roshanbisht1992@gmail.com>
2021-06-24 23:52:16 +0530

    divisions in Python

---
+++ day-4-python-division.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+print("Integer division =", a//b)
+print("Float division =", a/b)
+
+# Integer division or Floor division : The // operator first divides
+# the number on the left by the number on the right and then rounds down
+# to an integer.
+# Float division: division with the / operator always returns a float.
