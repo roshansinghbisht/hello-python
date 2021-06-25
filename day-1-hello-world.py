cd13453d43467d075ed61ccdea8e5421ad86afcc
roshansinghbisht <40305442+roshansinghbisht@users.noreply.github.com>
2021-06-25 19:35:52 +0530

    Updated the comments

--- day-1-hello-world.py
+++ day-1-hello-world.py
@@ -10,8 +10,8 @@ print("This is Roshan")
 # We see many other parameters here, but we passed only one in above
 # code: *objects = "Hello", "World".
 
-# If we see a little more closely, we see that in most of the parameters
-# the default arguments are provided using '='. Lets see then one by one
+# If we see a little more closely, we'll see that in most of the parameters
+# the default arguments are provided using '='. Lets see them one by one
 # sep= ' ' => this is the separator if multiple arguments are passed
 # using a comma. This separator tells python to provide a space (' ') in
 # between the 'objects'. We can see this in our code as well- this
@@ -19,9 +19,9 @@ print("This is Roshan")
 # end='\n' => this tells python what to do after the print statement is
 # executed. The '\n' is the newline character which inserts a new line
 # in the program. This is why in the output, we see 'This is Roshan'
-# getting printed in the next line/
-# Similarly for file=sys.stdout and flush=False) are the default
-# arguments.
+# getting printed in the next line.
+# Similarly file=sys.stdout and flush=False) too have the default
+# argument values.
 
 # So here we only passed the one argument (*objects'). We could have
-# skipped that as well, but then Python would have only printed the '\n'
+# skipped that as well, but then Python would have only printed the '\n'.
