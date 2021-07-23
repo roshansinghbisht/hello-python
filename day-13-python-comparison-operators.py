0c5863c5ebc224cb1cf5514887a3961596fc1039
Roshan Bisht <roshanbisht1992@gmail.com>
2021-07-23 14:16:33 +0530

    Added documentation for comparison and logical operators Python

--- day-13-python-comparison-operators.py
+++ day-13-python-comparison-operators.py
@@ -11,3 +11,21 @@ rio_de_janeiro_pop_density = rio_population/rio_area
 # and False otherwise
 
 print(san_francisco_pop_density > rio_de_janeiro_pop_density)
+
+
+# The bool data type holds one of the values True or False, which are
+# often encoded as 1 or 0, respectively.
+
+# There are 6 comparison operators that are common to see in order to
+# obtain a bool value:
+# < : Less Than
+# > : Greater Than
+# <= : Less Than or Equal To
+# >= : Greater Than or Equal To
+# == : Equal to
+# != : Not Equal to
+
+# And there are 3 logical operators you need to be familiar with:
+# and - Evaluates if all provided statements are True
+# or - Evaluates if at least one of many statements is True
+# not - Flips the Bool Value
