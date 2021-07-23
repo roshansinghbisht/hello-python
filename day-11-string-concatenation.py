2fa48c5cba56c68c5a8ee9a4059a55523781b4e0
Roshan Bisht <roshanbisht1992@gmail.com>
2021-07-23 14:00:18 +0530

    added code for string concatenation

---
+++ day-11-string-concatenation.py
@@ -0,0 +1,16 @@
+# You’ll be provided with example data for a user, the time of their
+# visit and the site they accessed. You should use the variables provided
+# and the techniques you’ve learned to print a log message like this one
+#  (with the username, url, and timestamp replaced with values from the
+#  appropriate variables):
+#
+# Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at
+# 16:20.
+
+username = "Ash"
+timestamp = "06:45"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+
+print(username, 'accessed the site', url, 'at', timestamp+'.')
