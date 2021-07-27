b9dcca00836f0b0f4e24a9525c9f65e683615486
Roshan Bisht <roshanbisht1992@gmail.com>
2021-07-27 23:59:26 +0530

    Adding code for type conversion python

---
+++ day-14-type-conversion.py
@@ -0,0 +1,16 @@
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+
+total= int(mon_sales)+int(tues_sales)+int(wed_sales)+int(thurs_sales)+int(fri_sales)
+total=str(total)
+print("This week's total sales: "+total)
+
+# weekly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+# weekly_sales = str(weekly_sales)  #convert the type back!!
+# print("This week's total sales: " + weekly_sales)
